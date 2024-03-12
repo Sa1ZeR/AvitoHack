933ffcd3cee082351aab03c12dd38cdcb59c2dba
bifurcated <litvinenko-maks@inbox.ru>
2024-03-13 00:06:51 +0300

    Delete example vue files

---
+++ analytic-web/src/components/PostForm.vue
@@ -0,0 +1,50 @@
+<template>
+    <form @submit.prevent>
+        <h4>Создание поста</h4>
+        <input 
+            v-bind:value="title"
+            @input="inputTitle" 
+            class="input" 
+            type="text" 
+            placeholder="Описание"
+        >
+        <input 
+            v-bind:value="body"
+            @input="inputBody" 
+            class="input" 
+            type="text" 
+            placeholder="Название"
+        >
+        <button 
+            class="btn" 
+        >
+            Создать
+        </button>
+    </form>
+</template>
+
+<script>
+export default {
+  name: "post-form"
+}
+</script>
+
+<style>
+form {
+    display: flex;
+    flex-direction: column;
+}
+.input {
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
+.btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    color: teal;
+    border: 1px solid teal;
+}
+</style>
