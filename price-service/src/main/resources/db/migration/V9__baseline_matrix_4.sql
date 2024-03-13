326d68eb78609c0c77c1935e54b2004ee42a79df
bifurcated <litvinenko-maks@inbox.ru>
2024-03-13 20:54:14 +0300

    Fix baseline_matrix_4 empty

---
+++ price-service/src/main/resources/db/migration/V9__baseline_matrix_4.sql
@@ -0,0 +1,8 @@
+create table if not exists baseline_matrix_4(
+     microcategory_id int,
+     location_id int,
+     price int
+);
+
+insert into baseline_matrix_4 (microcategory_id, location_id, price)
+values  (1, 1, 100);
